c0bbe72ce539d1e0ce6e8bee119c52998547e866
hardyzulhardiyanto <azulhardiyanto@gmail.com>
2019-12-21 19:49:34 +0700

    api server send 3002

--- client/src/helpers/accessAPI.js
+++ client/src/helpers/accessAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // if push please change to server comment other
-const API_URL = "http://52.76.198.78:3001/";
+const API_URL = "http://52.76.198.78:3002/";
 
 // localStorage
 // const API_URL = "http://localhost:3001/";
